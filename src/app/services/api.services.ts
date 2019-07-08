05354e5cb567907f4681dcc2b16be4b04e9cc4ba
George Almonte <georgeaf1825@gmail.com>
2019-07-07 20:15:10 -0400

    added server

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -1,28 +1,19 @@
-import {Injectable } from '@angular/core'
-import {HttpClient} from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 import { BehaviorSubject } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable()
-
-
 export class Api {
-    constructor( private http: HttpClient){}
-   
+    constructor(private http: HttpClient) { }
 
+    private _restaurants = new BehaviorSubject([]);
+    restaurants = this._restaurants.asObservable();
 
-  private _restaurants = new BehaviorSubject([]);
-  restaurants = this._restaurants.asObservable();
-    
     getRestaurants = (input, lat, long) => {
-        // return this.http.get('https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=restaurants&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours&locationbias=circle:2000@42.3314,-83.0458&minResults=20&key=AIzaSyAZ1xMLoKB963qGBjIuAtqLokfOIc-IpxQ');
-        let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+${input}?&location=${lat},${long}&radius=2000&key=AIzaSyAZ1xMLoKB963qGBjIuAtqLokfOIc-IpxQ`
-        
-        if(!environment.production)
-            url = `https://thingproxy.freeboard.io/fetch/` + url;
-        
-        return this.http.get(url);
+        const URL = `${environment.api}/restaurants/${input}/${lat}/${long}`;
+        return this.http.get(URL);
     }
 
     getLocationAPI = () => {
